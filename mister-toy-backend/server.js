13dd9f7f03d71a865ca3e768da99a8a4f3d86cec
ofer <ofergavri@gmail.com>
2023-01-10 23:24:41 +0200

    for deploy

--- mister-toy-backend/server.js
+++ mister-toy-backend/server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
 
 const toyService = require('./services/toy.service.js')
 const app = express()
 
-// App configuration
-app.use(express.static('public'))
-const corsOptions = {
-    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-    credentials: true
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 
@@ -80,4 +84,13 @@ app.delete('/api/toy/:toyId', (req, res) => {
         })
 })
 
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
+
